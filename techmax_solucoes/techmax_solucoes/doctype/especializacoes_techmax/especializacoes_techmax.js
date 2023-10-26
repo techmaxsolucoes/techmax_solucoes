51436b9cdd55ec850c20f67bf9c3e34d76a95f4f
LaviniaPedroso <lpedroso@techmaxsolucoes.com.br>
2023-10-26 19:55:40 +0000

    techmax-solucoes

---
+++ techmax_solucoes/techmax_solucoes/doctype/especializacoes_techmax/especializacoes_techmax.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, TechMax Soluções and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Especializacoes TechMax", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ techmax_solucoes/techmax_solucoes/doctype/membros_do_time/test_membros_do_time.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, TechMax Soluções and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMembrosdotime(FrappeTestCase):
+	pass
